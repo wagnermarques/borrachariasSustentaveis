a83062ea95bc33532fb4f43beeab20bf0926a05d
henriquegallodias <henrique0521@hotmail.com>
2019-06-21 01:25:11 -0300

    TELA DE CADASTRO FINALIZADA

--- BorrachariasSustentaveis/app/src/main/java/eteczl/edu/br/borrachariassustentaveis/ActivityCadastro.kt
+++ BorrachariasSustentaveis/app/src/main/java/eteczl/edu/br/borrachariassustentaveis/ActivityCadastro.kt
@@ -2,11 +2,40 @@ package eteczl.edu.br.borrachariassustentaveis
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_cadastro.*
 
 class ActivityCadastro : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro)
+
+        val btncadastrar = findViewById<Button>(R.id.btncadastrar) as Button
+        btncadastrar.setOnClickListener {
+            val toast = Toast.makeText(this@ActivityCadastro,"Botão cadastrar clicado...",Toast.LENGTH_LONG)
+            toast.setGravity(Gravity.TOP,0,0)
+            toast.show()
+
+        }
+
+        val btnclear = findViewById<Button>(R.id.btnlimpar) as Button
+        btnclear.setOnClickListener {
+            name.text.clear()
+            cep.text.clear()
+            fone.text.clear()
+            email.text.clear()
+            pass.text.clear()
+        }
+
+
+
+
+
     }
 }
