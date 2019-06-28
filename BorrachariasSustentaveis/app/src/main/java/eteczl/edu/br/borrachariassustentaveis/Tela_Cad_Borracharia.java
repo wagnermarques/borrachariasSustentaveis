aecfc87d7138f7b3d68a100c6179a2aac37555bd
ayodeleoduduwa <ayodeleoduduwa52@gmail.com>
2019-06-28 05:35:14 -0300

    tela de cadastroborracharia finalizada

---
+++ BorrachariasSustentaveis/app/src/main/java/eteczl/edu/br/borrachariassustentaveis/Tela_Cad_Borracharia.java
@@ -0,0 +1,13 @@
+package eteczl.edu.br.borrachariassustentaveis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tela_Cad_Borracharia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela__cad__borracharia);
+    }
+}
