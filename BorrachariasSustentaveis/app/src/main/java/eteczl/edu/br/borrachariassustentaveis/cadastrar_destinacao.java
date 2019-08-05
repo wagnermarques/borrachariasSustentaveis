20bd51bd48203c081cbb782a26ad09dd22152931
unknown <digabum@gmail.com>
2019-08-05 07:37:31 -0300

    Fazendo a tela cadastrar destinação

---
+++ BorrachariasSustentaveis/app/src/main/java/eteczl/edu/br/borrachariassustentaveis/cadastrar_destinacao.java
@@ -0,0 +1,26 @@
+package eteczl.edu.br.borrachariassustentaveis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class cadastrar_destinacao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_destinacao);
+
+                Button limpar = (Button) findViewById(R.id.btnlimpar);
+                Button salvar = (Button) findViewById(R.id.btnsalvar);
+
+
+
+
+
+
+    }
+}
