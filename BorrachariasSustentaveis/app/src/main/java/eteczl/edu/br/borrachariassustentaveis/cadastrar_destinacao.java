cfbb8f53c4d9cde43b7254f7db11c3b12a56dba4
vivianhds <vvnhenriquee@gmail.com>
2019-08-12 01:39:26 -0300

    Colocando ação no botão limpar

--- BorrachariasSustentaveis/app/src/main/java/eteczl/edu/br/borrachariassustentaveis/cadastrar_destinacao.java
+++ BorrachariasSustentaveis/app/src/main/java/eteczl/edu/br/borrachariassustentaveis/cadastrar_destinacao.java
@@ -5,18 +5,49 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
+import android.widget.EditText;
 
 public class cadastrar_destinacao extends AppCompatActivity {
+
+    EditText nome;
+    EditText descricao;
+
+    EditText cidade;
+    EditText uf;
+    EditText end;
+    EditText bairro;
+
+    Button limpar;
+    Button salvar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_destinacao);
+                limpar = (Button) findViewById(R.id.btnlimpar);
+                salvar = (Button) findViewById(R.id.btnsalvar);
 
-                Button limpar = (Button) findViewById(R.id.btnlimpar);
-                Button salvar = (Button) findViewById(R.id.btnsalvar);
+                nome = (EditText) findViewById(R.id.txtnome);
+        descricao = (EditText) findViewById(R.id.txtdesc);
+        cidade = (EditText) findViewById(R.id.txtcidade);
+        uf = (EditText) findViewById(R.id.txtuf);
+        end = (EditText) findViewById(R.id.txtend);
+        bairro = (EditText) findViewById(R.id.txtbairro);
 
+
+        limpar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                nome.setText("");
+                descricao.setText("");
+                cidade.setText("");
+                uf.setText("");
+                end.setText("");
+                bairro.setText("");
+
+            }
+        });
 
 
 
