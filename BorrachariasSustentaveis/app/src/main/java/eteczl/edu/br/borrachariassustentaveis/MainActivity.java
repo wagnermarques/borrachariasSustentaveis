58f24cfbc174cff0a9e600c0b1e1e357ec6df611
thcarvalho <th29.br@gmail.com>
2019-08-15 21:51:39 -0300

    listagem de usuarios

--- BorrachariasSustentaveis/app/src/main/java/eteczl/edu/br/borrachariassustentaveis/MainActivity.java
+++ BorrachariasSustentaveis/app/src/main/java/eteczl/edu/br/borrachariassustentaveis/MainActivity.java
@@ -1,101 +1,118 @@
 package eteczl.edu.br.borrachariassustentaveis;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.telephony.PhoneNumberUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+    List<String> opcoes;
+    ArrayAdapter<String>adaptador;
+    ListView listaOpcoes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        listaOpcoes = (ListView) findViewById(R.id.ListaOpcoes);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        opcoes = new ArrayList<String>();
+
+        opcoes.add("Carlos Eduardo");
+        opcoes.add("Fabricio");
+        opcoes.add("Maria");
+        opcoes.add("Carla");
+        opcoes.add("João");
+        opcoes.add("Thaian");
+
+        adaptador = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, opcoes);
+        listaOpcoes.setAdapter(adaptador);
+        listaOpcoes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0: carlosEduardo();
+                        break;
+                    case 1: fabricio();
+                        break;
+                    case 2: maria();
+                        break;
+                    case 3: carla();
+                        break;
+                    case 4: Joao();
+                        break;
+                    case 5: Thaian();
+                        break;
+                }
             }
         });
+    }
+    private void carlosEduardo(){
+        Intent nome1 = new Intent(MainActivity.this, MainActivity.class);
+        startActivity(nome1);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+        Uri ligarCarlos = Uri.parse("tel: 11 95685-1458");
+        Intent navegarCarlos = new Intent(Intent.ACTION_DIAL, ligarCarlos);
+        startActivity(navegarCarlos);
+    }
+    private void fabricio(){
+        Intent nome2 = new Intent(MainActivity.this, MainActivity.class);
+        startActivity(nome2);
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        Uri ligarFabricio = Uri.parse("tel: 11 98564-4584");
+        Intent navegarFabricio = new Intent(Intent.ACTION_DIAL, ligarFabricio);
+        startActivity(navegarFabricio);
+    }
+    private void maria(){
+        Intent nome3 = new Intent(MainActivity.this, MainActivity.class);
+        startActivity(nome3);
+
+        Uri ligarMaria = Uri.parse("tel: 11 95684-5236");
+        Intent navegarMaria = new Intent(Intent.ACTION_DIAL, ligarMaria);
+        startActivity(navegarMaria);
+    }
+    private void carla(){
+        Intent nome4 = new Intent(MainActivity.this, MainActivity.class);
+        startActivity(nome4);
+
+        Uri ligarCarla = Uri.parse("tel: 11 96582-2541");
+        Intent navegarCarla = new Intent(Intent.ACTION_DIAL, ligarCarla);
+        startActivity(navegarCarla);
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+    private void Joao(){
+        Intent nome5 = new Intent(MainActivity.this, MainActivity.class);
+        startActivity(nome5);
+
+        Uri ligarJoao = Uri.parse("tel: 11 55685-5214");
+        Intent navegarJoao = new Intent(Intent.ACTION_DIAL, ligarJoao);
+        startActivity(navegarJoao);
     }
 
-    @Override
+    private void Thaian(){
+        Intent nome6 = new Intent(MainActivity.this, MainActivity.class);
+        startActivity(nome6);
+
+        Uri ligarThaian = Uri.parse("tel: 11 65892-2541");
+        Intent navegarThaian = new Intent(Intent.ACTION_DIAL, ligarThaian);
+        startActivity(navegarThaian);
+    }
+
+   /* @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
+    }*/
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
 }
